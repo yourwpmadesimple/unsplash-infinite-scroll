d33c7c8ddf358a70965ca26130c5e21a03f307ea
Wayne Hatter <wayne@yourwpmadesimple.com>
2022-01-26 16:41:24 -0700

    finished

--- script.js
+++ script.js
@@ -2,12 +2,26 @@
 const imageContainer = document.getElementById("image-container");
 const imageLoader = document.getElementById("image-loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+let count = 5;
 const apiKey = "YQJH2cAXsTr2SYCvJ-s1z4NKdBAxQZ08eSj78Yq_QB8";
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function checkIfImageLoaded() {
+  imagesLoaded++;
+  console.log(imagesLoaded);
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    count = 30;
+    apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+  }
+}
 
 // Helper Function to Set Attributes on Elements
 function setAttributes(element, attributes) {
@@ -18,6 +32,9 @@ function setAttributes(element, attributes) {
 
 // Create Elements for links and photos, Add to DOM
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+
   // Run function for each photosArray
   photosArray.forEach((photo) => {
     // Create <a> element to link to Unsplash, Set attributes
@@ -40,6 +57,9 @@ function displayPhotos() {
       title: photo.alt_description,
     });
 
+    // Event Listener, Check when each image is loaded
+    img.addEventListener("load", checkIfImageLoaded);
+
     // Put <img> inside the <a> element, Then put both inside imageContainer
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -58,6 +78,17 @@ async function getPhotos() {
     console.log(error);
   }
 }
+
+// Check to see if scroll has reached bottom of page, load more photos
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 // On Load
 getPhotos();
